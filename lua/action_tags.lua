47808151537849b03b1848355657bdef0d9de9b4
Derek Hoagland <thegrickit@gmail.com>
2015-02-08 18:33:51 -0500

    Make [harvest_tree] actually increment the material stat now and take an override for how much material will be gained.

--- lua/action_tags.lua
+++ lua/action_tags.lua
@@ -2,18 +2,25 @@ local helper = wesnoth.require "lua/helper.lua"
 local wml_actions = wesnoth.wml_actions
 
 function wml_actions.harvest_tree(cfg)
-	local side = tostring(cfg.side)
+	local sides = wesnoth.get_sides(cfg)
 	local locations = wesnoth.get_locations(cfg)
+	local amount = cfg.amount or 10
 
 	for i, loc in ipairs(locations) do
 		local x, y = loc[1], loc[2]
 		if wesnoth.match_location(x, y, { terrain = "*^F*" }) then
 			wesnoth.scroll_to_tile(x, y, true)
 			wesnoth.play_sound('wose-die.ogg',false)
-			wesnoth.float_label(x, y, string.format( "<span foreground='#BCB088'>%s</span>", "+10 wood"))
 			wesnoth.set_terrain(x, y, "^", "overlay")
-			wesnoth.message('foobar', x .. ' ' ..y)
 			wml_actions.redraw {}
+
+			for i, side in ipairs(sides) do
+				local current_wood = wesnoth.get_variable(string.format("side_stats[%s].material", side.side)) or 0
+				local current_bonus = wesnoth.get_variable(string.format("side_bonuses[%s].tree", side.side)) or 0
+				local gain = amount + current_bonus
+				wesnoth.set_variable(string.format("side_stats[%s].material", side.side), (current_wood+gain))
+				wesnoth.float_label(x, y, string.format("<span foreground='#BCB088'>+%s material</span>", gain))
+			end
 		end
 	end
 end
